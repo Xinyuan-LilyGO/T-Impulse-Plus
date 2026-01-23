049729867ffde5ef6d262cbea7bee6a427d1b173
LILYGO_L <13073925292@163.com>
2026-01-23 15:03:39 +0800

    update the cpp_bus_driver library to use a safer method for parsing gnss

--- examples/gps_2/gps_2.ino
+++ examples/gps_2/gps_2.ino
@@ -2,65 +2,90 @@
  * @Description: GPS test
  * @Author: LILYGO_L
  * @Date: 2024-10-25 17:57:30
- * @LastEditTime: 2025-07-17 08:56:35
+ * @LastEditTime: 2026-01-23 15:01:14
  * @License: GPL 3.0
  */
 #include "cpp_bus_driver_library.h"
 #include "pin_config.h"
 #include <Adafruit_TinyUSB.h>
 
+#define MAX_UART_RX_BUFFER_SIZE 1024
+
 size_t CycleTime = 0;
+
+bool Gps_Positioning_Flag = false;
+size_t Gps_Positioning_Time = 0;
+
+auto Uart_Rx_Buffer = std::make_unique<uint8_t[]>(MAX_UART_RX_BUFFER_SIZE);
+size_t Uart_Rx_Count = 0;
 
 auto Nrf52840_Gnss = std::make_shared<Cpp_Bus_Driver::Gnss>();
 
 void setup()
 {
     Serial.begin(115200);
+
+    uint8_t serial_init_count = 0;
     while (!Serial)
     {
         delay(100); // wait for native usb
+        serial_init_count++;
+        if (serial_init_count > 30)
+        {
+            break;
+        }
     }
-    Serial.println("Ciallo");
 
     // 3.3V Power ON
     pinMode(RT9080_EN, OUTPUT);
     digitalWrite(RT9080_EN, HIGH);
 
+    pinMode(GPS_EN, OUTPUT);
+    digitalWrite(GPS_EN, LOW); // gps开启
+
     Serial2.setPins(GPS_UART_TX, GPS_UART_RX);
     Serial2.begin(38400);
-
-    pinMode(GPS_1PPS, INPUT);
 }
 
 void loop()
 {
+    while (Serial2.available() && Uart_Rx_Count < MAX_UART_RX_BUFFER_SIZE)
+    {
+        Uart_Rx_Buffer[Uart_Rx_Count] = Serial2.read();
+        Uart_Rx_Count++;
+    }
+
     if (millis() > CycleTime)
     {
-        // 检查Serial2是否有可用数据
-        if (Serial2.available())
+        if (Gps_Positioning_Flag == false)
         {
-            // 读取Serial2所有可用数据到缓冲区
-            const size_t bufferSize = 1024;
+            Gps_Positioning_Time += 5;
+        }
 
-            std::unique_ptr<uint8_t[]> buffer(new uint8_t[bufferSize]);
-            size_t bytesRead = 0;
-
-            while (Serial2.available() && bytesRead < bufferSize)
-            {
-                buffer[bytesRead++] = Serial2.read();
-            }
-
-            buffer[bytesRead] = '\0';
+        // 检查Serial2是否有可用数据
+        if (Uart_Rx_Count >= MAX_UART_RX_BUFFER_SIZE)
+        {
+            Uart_Rx_Buffer[MAX_UART_RX_BUFFER_SIZE - 1] = '\0';
 
             // 打印RMC的相关信息
-            printf("---begin---\n%s \n---end---\n", buffer.get());
+            printf("---begin---\n%s \n---end---\n", Uart_Rx_Buffer.get());
 
-            printf("------------RMC------------\n");
+            printf("---RMC---\n");
 
             // 创建Rmc对象用于存储解析结果
             Cpp_Bus_Driver::Gnss::Rmc rmc;
+
+            if (Gps_Positioning_Flag == false)
+            {
+                printf("Gps N:%ds\n", Gps_Positioning_Time);
+            }
+            else
+            {
+                printf("Gps Y:%d s\n", Gps_Positioning_Time);
+            }
+
             // 调用parse_rmc_info进行解码
-            if (Nrf52840_Gnss->parse_rmc_info(buffer.get(), bytesRead, rmc) == true)
+            if (Nrf52840_Gnss->parse_rmc_info(Uart_Rx_Buffer.get(), Uart_Rx_Count, rmc) == true)
             {
                 printf("location status: %s\n", (rmc.location_status).c_str());
 
@@ -82,6 +107,8 @@ void loop()
                            rmc.location.lat.degrees, rmc.location.lat.minutes, rmc.location.lat.degrees_minutes, (rmc.location.lat.direction).c_str());
                     rmc.location.lat.update_flag = false;
                     rmc.location.lat.direction_update_flag = false;
+
+                    Gps_Positioning_Flag = true;
                 }
                 if ((rmc.location.lon.update_flag == true) && (rmc.location.lon.direction_update_flag == true))
                 {
@@ -89,10 +116,22 @@ void loop()
                            rmc.location.lon.degrees, rmc.location.lon.minutes, rmc.location.lon.degrees_minutes, (rmc.location.lon.direction).c_str());
                     rmc.location.lon.update_flag = false;
                     rmc.location.lon.direction_update_flag = false;
+
+                    Gps_Positioning_Flag = true;
                 }
             }
+            else
+            {
+                printf("gps data: read fail\n");
+            }
+
+            Uart_Rx_Count = 0;
+        }
+        else
+        {
+            printf("Gps E:%ds\n", Gps_Positioning_Time);
         }
 
-        CycleTime = millis() + 1000;
+        CycleTime = millis() + 5000;
     }
 }
